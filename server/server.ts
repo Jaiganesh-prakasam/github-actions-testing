5fb3f25d660fc064ead2aa143c33a30cd96ef1f4
Jai g <jprakasam@ntst.com>
2024-04-25 13:19:20 +0530

    initial commit

---
+++ server/server.ts
@@ -0,0 +1,35 @@
+/* server.js in root directory */
+const fs = require('fs');
+const path = require('path');
+
+const dir = 'angular-app/src/environments';
+const file = 'environment.ts';
+
+const content = `${process.env['DEV_ENV_VARS']}`;
+
+fs.access(dir, fs.constants.F_OK, (err: any) => {
+  if (err) {
+    // Directory doesn't exist
+    console.log("src doesn't exist, creating now", process.cwd());
+    // Create /src
+    try {
+      fs.mkdirSync(dir, {recursive: true});
+    } catch (error) {
+      console.log(`Error while creating ${dir}. Error is ${error}`);
+      process.exit(1);
+    }
+  }
+  // Now write to file
+  try {
+    fs.writeFileSync(dir + '/' + file, content);
+    console.log('Created successfully in', process.cwd());
+    if (fs.existsSync(dir + '/' + file)) {
+      console.log('File is created', path.resolve(dir + '/' + file));
+      const str = fs.readFileSync(dir + '/' + file).toString();
+      console.log(str);
+    }
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+});
