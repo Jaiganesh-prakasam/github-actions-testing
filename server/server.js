430b1a227accaa16e776b7cdec8731d11397c04e
Jai g <jprakasam@ntst.com>
2024-04-25 18:18:35 +0530

    generating environment files for both the environments

--- server/server.js
+++ server/server.js
@@ -2,34 +2,38 @@
 const fs = require("fs");
 const path = require("path");
 
-const dir = "../angular-app/src/environments";
+const dir = "/src/environments";
 const file = "environment.ts";
 
 const content = `${process.env["DEV_ENV_VARS"]}`;
 
-fs.access(dir, fs.constants.F_OK, (err) => {
-  if (err) {
-    // Directory doesn't exist
-    console.log("src doesn't exist, creating now");
-    // Create /src
+function generateEnvironments(dir) {
+  fs.access(dir, fs.constants.F_OK, (err) => {
+    if (err) {
+      // Directory doesn't exist
+      console.log("src doesn't exist, creating now");
+      // Create /src
+      try {
+        fs.mkdirSync(dir, { recursive: true });
+      } catch (error) {
+        console.log(`Error while creating ${dir}. Error is ${error}`);
+        process.exit(1);
+      }
+    }
+    // Now write to file
     try {
-      fs.mkdirSync(dir, { recursive: true });
+      fs.writeFileSync(dir + "/" + file, content);
+      if (fs.existsSync(dir + "/" + file)) {
+        console.log("File is created", path.resolve(dir + "/" + file));
+        const str = fs.readFileSync(dir + "/" + file).toString();
+        console.log(str);
+      }
     } catch (error) {
-      console.log(`Error while creating ${dir}. Error is ${error}`);
+      console.error(error);
       process.exit(1);
     }
-  }
-  // Now write to file
-  try {
-    fs.writeFileSync(dir + "/" + file, content);
-    if (fs.existsSync(dir + "/" + file)) {
-      console.log("File is created", path.resolve(dir + "/" + file));
-      console.log("----> content", content, "----> content");
-      const str = fs.readFileSync(dir + "/" + file).toString();
-      console.log(str);
-    }
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
-});
+  });
+}
+
+generateEnvironments('../angular-app' + dir);
+generateEnvironments('../code-ui' + dir);
